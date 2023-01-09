2ea8e4f33e298211858ea176dc22b60b624b613d
xieyongcheng <752106133@qq.com>
2023-01-09 16:12:15 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    WindiCSS(),
+    eslintPlugin({
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"]
+    })
+  ]
+});
